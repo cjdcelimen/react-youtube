1278f9e006f7f44219688a0a8a12aa9f70889939
CJ Celimen <cjdcelimen@gmail.com>
2020-12-03 20:23:16 +0800

    Update Components - Search and Videos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { SearchBar, VideoList, VideoDetail } from "./components";
+import Youtube from "./api/Youtube";
 
-function App() {
+export default () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
   return (
-    <div className="App">
-      <h1>React Youtube</h1>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
 
-export default App;
+  async function handleSubmit(searchTerm) {
+    const { data: { items: videos } } = await Youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: 'personalKey',
+        q: searchTerm,
+      }
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
+}
